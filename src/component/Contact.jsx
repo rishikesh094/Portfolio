5b0aff1d1920c0ed551437b04162d202537bfd57
Rishikesh Singh <rishikeshs820@gmail.com>
2025-05-07 12:26:43 +0530

    first commit

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -6,7 +6,7 @@ function Contact() {
         <div id='contact' className='w-full bg-gray-100 min-h-screen flex flex-col items-center py-8 px-4 md:space-y-10'>
             <h1 className='uppercase text-2xl tracking-widest border-b-4 border-gray-500 font-bold px-10 py-2 '>Contact</h1>
             <div className='md:w-1/2 py-8 text-justify'>
-                <p>If you are interested in my portfolio or would like to hire me for your project, please contact me via email (vbvber) or use the form below.</p>
+                <p>If you are interested in my portfolio or would like to hire me for your project, please contact me via email <a href="mailto:rishikeshs820@gmail.com">(rishikeshs820@gmail.com)</a> or use the form below.</p>
             </div>
             <div className='invert flex justify-center items-center my-8'>
                 <img src="assets/separatorWhite.png" alt="" />
